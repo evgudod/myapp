c63a62af537f372542a7e923bc951e80f20b1e8a
udod <udod@ua.fm>
2017-07-07 23:43:07 +0300

    last changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'this is a per and';
+  getter = "hi MAC";
 }
